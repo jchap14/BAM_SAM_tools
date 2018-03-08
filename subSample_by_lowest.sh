737df8b6a72613cd25d50c5852001966b1241993
James <jchap14@gmail.com>
2018-03-07 19:54:10 -0800

    subsample BAMs

---
+++ subSample_by_lowest.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+##### Generate BAM files with equal number of reads in each
+
+##### bash subSample_by_lowest.sh $BAMLIST
+
+## $BAMLIST = a newline separated textfile of BAMs (subsampled to equal read #)
+
+## add modules
+module add MACS2
+
+## define variables
+BAMLIST=`echo $1`
+NAME=`basename $BAMLIST .BAMlist`
+
+## write a tempscript to be looped over
+cat > $NAME.tempscript.sh << EOF
+#!/bin/bash
+#$ -N $NAME.subSampBAM
+#$ -j y
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1G
+#$ -pe shm 12
+#$ -l h_rt=5:59:00
+#$ -l s_rt=5:59:00
+
+########## Commands
+## create an empty file called $NAME.lineCount
+touch $NAME.lineCount
+## count the # of alignments in each BAM
+for line in \`cat $BAMLIST\`
+do
+samtools view \$line | wc -l >> $NAME.lineCount 
+done
+## find the BAM with the lowest # & set it as a variable
+MIN=\`cat $NAME.lineCount | sort -n | head -1\`
+## subsample each BAM to the MIN # of reads
+for line in \`cat $BAMLIST\`
+do
+macs2 randsample -t \$line -o \$line.subSamp.bam -n $MIN 
+##
+EOF
+
+## qsub then remove the tempscript
+qsub $NAME.tempscript.sh 
+sleep 1
+# rm $NAME.tempscript.sh
