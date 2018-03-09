cf87a0b9c078d0a5c851d524402a8304836045f4
James <jchap14@gmail.com>
2018-03-08 18:03:10 -0800

    initial

---
+++ mergeBAMs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+##### Generate BAM files with equal number of reads in each
+
+##### bash mergeBAMs.sh $BAMLIST
+
+## $BAMLIST = a newline separated textfile of BAMs
+
+## add modules
+
+## define variables
+BAMLIST=`echo $1`
+NAME=`basename $BAMLIST .BAMlist`
+
+## write a tempscript to be looped over
+cat > $NAME.tempscript.sh << EOF
+#!/bin/bash
+#$ -N $NAME.mergeBAMs
+#$ -j y
+#$ -cwd
+#$ -V
+#$ -l h_vmem=1G
+#$ -pe shm 12
+#$ -l h_rt=5:59:00
+#$ -l s_rt=5:59:00
+
+########## Commands
+## merge BAMs
+bamtools merge -b $BAMLIST $NAME.avgSubSamp.bam
+##
+EOF
+
+## qsub then remove the tempscript
+qsub $NAME.tempscript.sh 
+sleep 1
+# rm $NAME.tempscript.sh
